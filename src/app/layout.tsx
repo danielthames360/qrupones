b04d14a3b65989c1d561c98f922d70f32f6d3b52
Daniel Cespedes <danielthames360@gmail.com>
2023-12-02 08:50:58 -0400

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,43 @@ export const metadata: Metadata = {
   title: 'QRupones - Potencia la ReCompra y Fidelización de Clientes',
   description:
     'Escanea, ahorra, disfruta. Impulsando lealtad, generando ahorro. Incentiva a tus clientes a volver a tu negocio, beneficiándolos con cupones de descuentos para premiar su fidelidad e incentivar las recompras!',
+  keywords: [
+    'Qrupones',
+    'Recompra',
+    'Fidelización',
+    'Cupones de descuento',
+    'Ofertas y promociones',
+    'Programas de fidelización',
+    'Cupones digitales',
+    'Descuentos para clientes',
+    'Incentivos de recompra',
+    'Promociones para negocios',
+    'Ahorro y ofertas',
+    'Programas de lealtad',
+    'Cupones para tiendas',
+  ],
+  openGraph: {
+    type: 'website',
+    locale: 'es_ES',
+    url: 'https://qrupones.com/',
+    siteName: 'Qrupones',
+    images: [
+      {
+        url: '/public/images/landing/logo-header.png',
+        width: 962,
+        height: 256,
+        alt: 'Qrupones',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'QRupones - Potencia la ReCompra y Fidelización de Clientes',
+    description:
+      'Escanea, ahorra, disfruta. Impulsando lealtad, generando ahorro. Incentiva a tus clientes a volver a tu negocio, beneficiándolos con cupones de descuentos para premiar su fidelidad e incentivar las recompras!',
+    creator: '@danielthames',
+    images: ['/public/images/landing/logo-header.png'],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
