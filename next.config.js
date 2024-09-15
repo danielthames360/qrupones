b203483345fdd28f89aa50ecbb933e2ce0171d6f
Daniel Cespedes <danielthames360@gmail.com>
2024-09-15 18:17:10 -0400

    Remote origins for images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
